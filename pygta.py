02b8520f950824e17580ad8bad5871feb8026991
dmoneyballer <drew.gilliesbc@gmail.com>
2019-11-07 07:05:29 -0700

    faster captures

--- pygta.py
+++ pygta.py
@@ -11,12 +11,12 @@ import time
 last_time = time.time()
 while True:
     # grabbing full screen takes over a second, grabbinb 800x600 takes .35 seconds
-    printscreen_pil = ImageGrab.grab(bbox=(0,40,800,600))
-    printscreen_numpy = np.array(printscreen_pil.getdata(), dtype='uint8')\
-    .reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))
+    screen = ImageGrab.grab(bbox=(0,40,1920,1080))
+    # printscreen_numpy = np.array(printscreen_pil.getdata(), dtype='uint8')\
+    # .reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))
     print( time.time() - last_time)
     last_time = time.time()
-    cv2.imshow('window', printscreen_numpy)
+    cv2.imshow('window', cv2.cvtColor(np.array(screen), cv2.COLOR_BGR2RGB))
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindow()
         break
